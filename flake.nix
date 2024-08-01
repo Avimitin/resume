39c093163f418ce613f0c4f34accafa718d4c71d
Avimitin <dev@avimit.in>
2024-08-01 22:47:53 +0800

    nix: add typst env
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Generic devshell setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    let
+      overlay = import ./nix/overlay.nix;
+    in
+    {
+      # System-independent attr
+      inherit inputs;
+      overlays.default = overlay;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { overlays = [ overlay ]; inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        legacyPackages = pkgs;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.typst ];
+        };
+      });
+}
